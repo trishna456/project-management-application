a7ff7cc66bd5d792f8c066c662e9ded8a3e08b27
Trishna <trishna.patil13@gmail.com>
2024-04-12 12:56:10 -0400

    slidebar, homepage, add project screens

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import NewProject from "./components/NewProject";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from './components/NoProjectSelected'
+import { useState } from "react";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData, 
+        id:Math.random()
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject}/>
+  }
+  else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
+    </main>
+  
+  );
+}
+
+export default App;
